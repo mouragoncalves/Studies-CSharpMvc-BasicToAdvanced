eb4caa5841874c5c0541249cd102e6e8586593e0
IATec - Marcelo Moura Goncalves <marcelo.goncalves@iatec.com>
2025-03-25 23:54:19 -0300

    Ferramentas de Front-End, Bundding e Minification

---
+++ src/BasicMvc.WebApplication/wwwroot/js/site.js
@@ -0,0 +1,67 @@
+﻿function ZipCodeSearch() {
+    $(document).ready(function () {
+
+        function limpa_formulário_cep() {
+            // Limpa valores do formulário de cep.
+            $("#Address_Street").val("");
+            $("#Address_Neighborhood").val("");
+            $("#Address_City").val("");
+            $("#Address_FederalUnit").val("");
+        }
+
+        //Quando o campo cep perde o foco.
+        $("#Address_ZipCode").blur(function () {
+
+            //Nova variável "cep" somente com dígitos.
+            var cep = $(this).val().replace(/\D/g, '');
+
+            //Verifica se campo cep possui valor informado.
+            if (cep != "") {
+
+                //Expressão regular para validar o CEP.
+                var validacep = /^[0-9]{8}$/;
+
+                //Valida o formato do CEP.
+                if (validacep.test(cep)) {
+
+                    //Preenche os campos com "..." enquanto consulta webservice.
+                    $("#Address_Street").val("...");
+                    $("#Address_Neighborhood").val("...");
+                    $("#Address_City").val("...");
+                    $("#Address_FederalUnit").val("...");
+
+                    //Consulta o webservice viacep.com.br/
+                    $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?",
+                        function (dados) {
+
+                            if (!("erro" in dados)) {
+                                //Atualiza os campos com os valores da consulta.
+                                $("#Address_Street").val(dados.logradouro);
+                                $("#Address_Neighborhood").val(dados.bairro);
+                                $("#Address_City").val(dados.localidade);
+                                $("#Address_FederalUnit").val(dados.uf);
+                            } //end if.
+                            else {
+                                //CEP pesquisado não foi encontrado.
+                                limpa_formulário_cep();
+                                alert("CEP não encontrado.");
+                            }
+                        });
+                } //end if.
+                else {
+                    //cep é inválido.
+                    limpa_formulário_cep();
+                    alert("Formato de CEP inválido.");
+                }
+            } //end if.
+            else {
+                //cep sem valor, limpa formulário.
+                limpa_formulário_cep();
+            }
+        });
+    });
+}
+
+$(document).ready(function () {
+    $(".alert").fadeOut(3000);
+})
