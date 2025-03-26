bdd03233f15995ea6f75c2c91de6644f31128d10
IATec - Marcelo Moura Goncalves <marcelo.goncalves@iatec.com>
2025-03-25 21:00:00 -0300

    Configurar Program.cs para MVC

--- src/BasicMvc.WebApplication/Program.cs
+++ src/BasicMvc.WebApplication/Program.cs
@@ -1,7 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
